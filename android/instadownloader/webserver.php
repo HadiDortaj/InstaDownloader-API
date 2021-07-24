43fc28e4298a0d0e39c37ac8122cac450e9b21ea
Hadi-Dortaj <hadi.developer.m@gmail.com>
2021-07-25 00:51:53 +0430

    Remove php files and add a new json file

--- android/instadownloader/webserver.php
+++
@@ -1,5 +0,0 @@
-<?php
-$flavor = $_GET['flavor'];
-$appVersionCode = $_GET['appVersionCode'];
-header("Location: http://localhost/HappyDeveloper/android/instadownloader/api/v1/webserver.php?flavor=$flavor&appVersionCode=$appVersionCode");
-exit();
